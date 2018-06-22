d49cdc87e78599ce81b5989152ebd5ed1a045478
NazarkO <renkonazbka@gmail.com>
2018-06-22 18:52:43 +0300

    Last commit. Did third task.

--- index.js
+++ index.js
@@ -5,16 +5,40 @@ window.onload = init;
 function init(){
     fetch(studentUrl)
         .then(res => res.json())
-        .then(renderStudents)
+        .then(renderStudents);
+
+    let studentsForm = document.getElementById('student-form');
+    studentsForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+        createStudent()
+            .then(loadStudents)
+            .then(renderStudents);
+    })
 }
 
+function loadStudents(){
+    return fetch(studentUrl)
+        .then(res => res.json());
+}
+
+function createStudent(){
+    let student = {};
+    student.name = document.getElementById('student-form').name.value;
+    student.surname = document.getElementById('student-form').surname.value;
+    student.graduate = document.getElementById('student-form').graduate.value;
+
+    return fetch(studentUrl, {
+        method: 'POST',
+        headers: {'Content-Type':'application/json'},
+        body: JSON.stringify(student)
+    });
+}
 
 /*renderStudents([
     {name: "Nazar", surname: "Poshtarenko", graduate: 4},
     {name: "Sasha", surname: "Kotov", graduate: 5},
     {name: "Oleg", surname: "Nosurname", graduate: 2}
     ]);*/
-
 
 function renderStudents(students){
     let template = document.getElementById('student-template');
